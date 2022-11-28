69852ed4ec147b20359c99db7cd25bf16b388173
AvisTheAvaxion <avistheavaxion@gmail.com>
2022-11-28 15:45:06 -0500

    UserDatabase

---
+++ UserData/UserDBManager.java
@@ -0,0 +1,25 @@
+public class UserDBManager {
+  private String filePath;
+
+  private int AccNum;
+
+  public void saveUser(User user) {
+    
+  }
+
+  public User loadUser(String username, String password) {
+    return null;
+  }
+
+  public User[] searchUsers(String field, User employee) {
+    return null;
+  }
+
+  public boolean resetPassword(string username, string email, string newPassword) {
+    return false;
+  }
+
+  public User createUser(String firstName, String lastName, String phoneNumber, String email, String address, String username, String password) {
+    return null;
+  }
+}
