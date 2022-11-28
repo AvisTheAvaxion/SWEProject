05edfc1c0cad9f3991ef577508553b4df4d42f13
Avis TheAvaxion <avistheavaxion@gmail.com>
2022-11-28 19:56:48 +0000

    Client Class

---
+++ Webpages/Client.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class Client {
+  public User ActiveUser;
+  public JPanel currentPage;
+  public Order shoppingCart;
+  public boolean staySignedIn;
+
+  public boolean signIn(String username, String password) {
+    return false;
+  }
+
+  public void signOut() {
+    
+  }
+
+  public void Open() {
+    
+  }
+
+  public void Close() {
+    
+  }
+}
